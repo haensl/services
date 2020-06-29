f3eeb52d3c042d6f6a2868ed8ff5ef616b766a9e
HP Dietz <hp@jandh.org>
2020-06-29 19:15:41 +0200

    Avoid conflict with create-react-app default test.

--- src/package-tests/esm/esm.test.js
+++ src/package-tests/esm/esm.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import TestComponent from './TestComponent';
 
 describe('esm module test', () => {
   it('renders without crashing', () => {
-    expect(render.bind(render, <App />))
+    expect(render.bind(render, <TestComponent/>))
       .not
       .toThrow();
   });
